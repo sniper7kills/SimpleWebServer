53ef9a671538e271249fac151ba767275856437b
Will G <sniper7kills@users.noreply.github.com>
2018-11-03 19:58:14 -0700

    Update Main.cpp

--- CS225-Project/Main.cpp
+++ CS225-Project/Main.cpp
@@ -1,13 +1,29 @@
-/*
-	TCP Echo server example in winsock
-	Live Server on port 8888
-*/
-#include<stdio.h>
+#include <iostream>
+#include <string>
 #include "Webserver.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-	Webserver server(8888, "C:\\Users\\User\\OneDrive\\School Work\\Fall 2018\\CS 225\\Project\\CS225-Project\\CS225-Project\\HTML_FILES", 64);
+	//Need to verify argument passing works properly
+	int port,maxClients;
+	string htmlDir;
+	if(argc < 3){
+		cout << "USAGE: server.exe <port> <html_dir> <max_clients>";
+		exit(0);
+	}
+	port = argv[1];
+	htmlDir = argv[2];
+	if(argc < 4){
+		maxClients=64;
+	}else{
+		maxClients = argv[3];
+	}
+	
+	Webserver server(port, htmlDir, maxClients);
 	server.init();
 	server.run();
+	
+	return 0;
 }
